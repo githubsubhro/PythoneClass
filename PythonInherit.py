313c471ccc1097026266ad0a5225098251c3a589
subhro <suvro.nbu@gmail.com>
2021-03-12 23:05:25 +0530

    code modified

--- PythonInherit.py
+++ PythonInherit.py
@@ -1,12 +1,25 @@
 class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+        print(self)
 
-  def printname(self):
-    print(self.firstname, self.lastname)
+    def printname(self):
+        print(self, self.firstname, self.lastname)
 
-#Use the Person class to create an object, and then execute the printname method:
 
-x = Person("Subhro", "Doe")
-x.printname() 
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+
+# Use the Person class to create an object, and then execute the printname method:
+
+x = Student("Subhro", "Doe", "1990")
+print(x)
+x.printname()
+x.welcome()
